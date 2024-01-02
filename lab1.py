70649588382ff9e118bfd1a2716a93f210ff4dca
Rekurnat <x.mikael.lindstrom@gmail.com>
2024-01-02 14:41:19 +0100

    Calculate age from dob

--- lab1.py
+++ lab1.py
@@ -1,8 +1,12 @@
 #Ask for name and greet person
 first = input('Enter your name:\n')
-print('Hello ' + first)
+print(f'Hello {first}')
 
 #Ask for last name
 last = input('What is your last name?:\n')
-print('Nice to meet you ' + first + ' ' + last)
+print(f'Nice to meet you {first} {last}')
 
+#Multiply two numbers
+number1 = int(input('Enter a number:\n'))
+number2 = int(input('And another number:\n'))
+print(f'Multiplying the two numbers equals {number1*number2}')
