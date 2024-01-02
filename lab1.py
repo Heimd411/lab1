12b9031481722f4ebe2fe280eb9c837df9e480d7
Rekurnat <x.mikael.lindstrom@gmail.com>
2024-01-02 14:18:36 +0100

    Add: promt for last name

--- lab1.py
+++ lab1.py
@@ -1,2 +1,8 @@
+#Ask for name and greet person
 first = input('Enter your name:\n')
 print('Hello ' + first)
+
+#Ask for last name
+last = input('What is your last name?:\n')
+print('Nice to meet you ' + first + ' ' + last)
+
