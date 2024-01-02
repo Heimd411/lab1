983befd6224b09ff8f225a92805e78637b125eb5
Rekurnat <x.mikael.lindstrom@gmail.com>
2024-01-02 14:45:49 +0100

    Moms påslag

--- lab1.py
+++ lab1.py
@@ -1,21 +1,23 @@
 import datetime
 
 #Ask for name and greet person
-first = input('Enter your name:\n')
+#first = input('Enter your name:\n')
+first = "John"
 print(f'Hello {first}')
 
 #Ask for last name
-last = input('What is your last name?:\n')
+#last = input('What is your last name?:\n')
+last = "Doe"
 print(f'Nice to meet you {first} {last}')
 
 #Multiply two numbers
 number1 = int(input('Enter a number:\n'))
 number2 = int(input('And another number:\n'))
-print(f'Multiplying the two numbers equals {number1*number2}')
+print(f'Multiplying the two numbers equals {number1*number2}\n')
 
 #Divide two numbers
-print(f'If we divide both numbers we get {number1/number2}')
+print(f'If we divide both numbers we get {number1/number2}\n')
 
 #Age from dob
 yearborn = 1977
-print(f'If you were born in {yearborn} then you are {datetime.datetime.today().year - yearborn} years old.')
+print(f'If you were born in {yearborn} then you are {datetime.datetime.today().year - yearborn} years old.\n')
