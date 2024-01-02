ef74b53d5e46fa67fa83f10916db03e3a5d01a15
Rekurnat <x.mikael.lindstrom@gmail.com>
2024-01-02 14:43:58 +0100

    Replaced some input prompts with static values

--- lab1.py
+++ lab1.py
@@ -1,3 +1,5 @@
+import datetime
+
 #Ask for name and greet person
 first = input('Enter your name:\n')
 print(f'Hello {first}')
@@ -10,3 +12,10 @@ print(f'Nice to meet you {first} {last}')
 number1 = int(input('Enter a number:\n'))
 number2 = int(input('And another number:\n'))
 print(f'Multiplying the two numbers equals {number1*number2}')
+
+#Divide two numbers
+print(f'If we divide both numbers we get {number1/number2}')
+
+#Age from dob
+yearborn = 1977
+print(f'If you were born in {yearborn} then you are {datetime.datetime.today().year - yearborn} years old.')
